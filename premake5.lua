4de279b6861aa46b64f5301d3ccb90bc2b9deae8
Henry Roach <129221637+hankwr@users.noreply.github.com>
2024-05-08 11:41:43 -0500

    generated files to run with wally project

--- premake5.lua
+++ premake5.lua
@@ -60,10 +60,3 @@ project "GLFW"
     
     filter "configurations:Distribution"
         symbols "Off"
-        -- figure out macros that need to be defined for generating static library on windows
-        -- _WIN32 is involved somewhere...
-
-        -- what configuration settings does GLFW want to compile??
-        -- remember: the configuration is defined by the class, but project-specific macros are not
-        -- there probably will not be a difference between debug/release/distribution for GLFW but idk
-        -- i don't know what GLFW would like for linux or macios systems
